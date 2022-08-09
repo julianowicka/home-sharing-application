904345354fcf2c7915cfc68a58128670f6e6d4ac
Julia Nowicka <julia.nowickaa@gmail.com>
2022-08-09 12:01:58 +0200

    Create GET and POST express routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,27 @@
 import express from 'express';
+import {listings} from "./listings";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 9000;
-const one = 1;
-const two= 2;
 
-app.get('/', (req, res) => res.send(`1 + 2 = ${one + two}`));
+app.use(bodyParser.json());
+
+app.get("/listings", ( _req, res) =>{
+    return res.send(listings);
+    });
+
+app.post("/delete-listings", (req, res) =>{
+    const id: string = req.body.id;
+
+    for (let i =0; i< listings.length; i++){
+        if (listings[i].id === id){
+            return res.send(listings.splice(i, 1));
+        }
+    }
+    return res.send("failed to delete listing");
+});
+
 app.listen(port);
+
 console.log(`[app]: http://localhost:${port}`);
